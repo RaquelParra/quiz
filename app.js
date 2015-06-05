ebbad1d291e96fd6c0ca9e5e5815da9dab75a134
sireck <raquel.parra@gmail.com>
2015-06-05 13:20:16 +0200

    identificador de version

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
+app.use('/',routes);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -21,9 +21,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(favicon(_dirname+'/public/favicon.ico');
+app.use(logger('dev');
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -58,4 +58,4 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
-app.listen(3000);
+//app.listen(3000);
