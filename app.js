f20b5be98e60882521038a612b06053e1520d66d
sireck <raquel.parra@gmail.com>
2015-06-08 11:07:04 +0200

    primera pagina y favicon

--- app.js
+++ app.js
@@ -15,14 +15,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(_dirname+'/public/favicon.ico');
-app.use(logger('dev');
+//app.use(favicon(_dirname+'/public/favicon.ico');
+//app.use(logger('dev');
 app.use('/', routes);
 
 // catch 404 and forward to error handler
