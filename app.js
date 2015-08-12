94d27d90725b4aeb18686dce1396269b8972d02f
sireck <raquel.parra@gmail.com>
2015-08-12 13:43:58 +0200

    control sesion

--- app.js
+++ app.js
@@ -6,44 +6,78 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var partials = require('express-partials');
 var methodOverride = require('method-override');
-var session= require('express-session');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 
 var app = express();
 
-//app.use('/',routes);
+var isFromSession = false;
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//app.use('/', routes);
 app.use(partials());
+
 // uncomment after placing your favicon in /public
-app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser('Quiz 2015'));
-app.use(session());
+app.use(session({ 
+            name: 'quiz-2015', // configuración de la cookie
+            secret: 'parra11',
+            resave: true,       // Forces the session to be saved back to the session store
+            rolling: true,      // Force a cookie to be set on every response. This resets the expiration date.
+            saveUninitialized: false,       //
+            cookie: { maxAge: 60000}  // Tiempo de la sesion, expiración de la cookie -> 30000 = 1min.
+            }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Helpers dinamicos
+
+
 app.use(function(req, res, next) {
 
-  if (!req.path.match(/\/login|\/logout/)){
-    req.session.redir= req.path;
-  }
+    // Hacer visible req.session en las vistas
+    req.session.touch();
+    res.locals.session = req.session;  
+    // guarda path en session.redir para despues de login
+    if(!req.path.match(/\/login|\/logout/)){
+        req.session.redir = req.path;       
+    }
+    
+    if(req.session.user){
+        isFromSession = true;
 
-  res.locals.session = req.session;
-  next();
+    }else{
+        if(isFromSession){
+            isFromSession = false;
+            var err = new Error('Sesión Finalizada');
+            err.status = 1001;
+           //next(err);          
+        }
+    }
+
+
+    next();
 });
+
+
 app.use('/', routes);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found !!!!!');
+    var err = new Error('Not Found');
     err.status = 404;
     next(err);
+
 });
 
 // error handlers
@@ -55,9 +89,9 @@ if (app.get('env') === 'development') {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
-            error: err,
-            errors: []
+            error: err, errors: []
         });
+        
     });
 }
 
@@ -67,11 +101,10 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
-        error: {},
-        errors: []
+        error: err, errors: []
     });
+    
 });
 
 
 module.exports = app;
-//app.listen(3000);
--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,41 +1,51 @@
-//MW de autorizacion de accesos HTTP restringidos
-exports.loginRequired = function (req, res, next) {
-  if (req.session.user) {
-    next();
-  } else {
-    res.redirect('/login');
-  }
+
+exports.loginRequired = function(req, res, next){
+	if(req.session.user){
+		next();
+	}else{
+		res.redirect('/login');
+	}
 };
 
-//Get login --formulario de login
-exports.new = function(req, res) {
-  var errors = req.session.errors || {};
-  req.session.errors = {};
 
-  res.render('sessions/new', {errors: errors});
+
+
+// GET /login new
+exports.new = function(req, res){
+	var errors = req.session.errors || {};
+	req.session.errors = {};
+
+	res.render('sessions/new',{errors: errors});	
 };
 
-//POST login crear sesion
-exports.create = function(req, res) {
-  var login    = req.body.login;
-  var password = req.body.password;
+// POST /login
+exports.create = function(req, res){
+	var login = req.body.login;
+	var password = req.body.password;
 
-  var userController = require('./user_controller');
-  userController.autenticar(login, password, function(error, user){
-     if (error){
-        req.session.errors = [{"message" : 'Se ha producido un error: ' + error}];
-        res.redirect("/login");
-        return
-     }
-     //crear req.session.user y guardar campos id y username
-     //la sesion se define por la existencia de req.session.user
-     req.session.user = {id: user.id, username: user.username};
-     res.redirect(req.session.redir.toString());//redireccion a path anterior a login
-  });
+	var userController = require('./user_controller');
+	userController.autenticar(login, password, function(error, user){
+
+		if(error){
+			req.session.errors = [{"message": 'Se ha producido un error:'+ error}];
+			res.redirect("/login");
+			return;
+		}
+
+		req.session.user = {id:user.id, username:user.username};
+
+		res.redirect(req.session.redir.toString());
+
+	});
 };
 
-//DELETE logout
+
+// DELETE /logout
 exports.destroy = function(req, res){
-   delete req.session.user;
-   res.redirect(req.session.redir.toString());
+	
+	if(req.session.user){
+		delete req.session.user;
+	}
+	res.redirect(req.session.redir.toString());
 };
+
